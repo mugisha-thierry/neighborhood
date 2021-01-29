7097bd75918d7ac6dc79e77b6bab3c8d78f9cd6b
Mugisha Thierry <thierrymugis@gmail.com>
2021-01-29 16:03:54 +0200

    created a decorator file

--- app/decorators.py
+++ app/decorators.py
@@ -3,7 +3,7 @@ from django.shortcuts import redirect
 
 
 
-def allowed_users(allowed_roles[]):
+def allowed_users(allowed_roles=[]):
     def decorator(view_func):
         def wrapper_func(request,*args,**kwargs):
             group = None
@@ -24,22 +24,17 @@ def admin_only(view_func):
         if request.user.groups.exists():
             group = request.user.groups.all()[0].name
 
-        if group == 'normal_users':
-            return redirect('home')
 
         if group == 'neighbourhood_admin':
-            return redirect('')    
+            return redirect('neighbourhood_admin')
 
-        if group =='system_administrator':
+        elif group == 'system_administrator':
+            return redirect('system_admin')     
+
+        elif group =='normal_users':
             return view_func(request,*args,**kwargs)
 
+        else:
+            return redirect ('login')    
+
     return wrapper_function                    
-
-# def (view_func):
-#     def wrapper_func(request,*args,**kwargs):
-#         if request.user.is_authenticated:
-#             return redirect ('home')
-#         else:
-#             return view_func(request,*args,**kwargs)
-
-#     return wrapper_func    
